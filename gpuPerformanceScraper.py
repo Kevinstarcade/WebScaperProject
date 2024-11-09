eb23d2667d8ea4121d815586136d7bf379c26229
Kevin Xiao <k.xiao3@share.epsb.ca>
2024-11-09 02:52:13 -0700

    began integrating databases

--- gpuPerformanceScraper.py
+++ gpuPerformanceScraper.py
@@ -2,29 +2,40 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-# open the driver in chrome
-# options = webdriver.ChromeOptions()
-# options.add_argument("--ignore-certificate-errors")
-# options.add_argument("--ignore-certificate-errors-spki-list")
-# options.add_argument("--ignore-ssl-errors")
-# options.add_argument("--allow-insecure-localhost")
-# options.add_argument("--disable-web-security")
-# driver = webdriver.Chrome(options=options)
-# driver.get("https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html")
-
 # define soup
-url = requests.get("https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html")
-soup = BeautifulSoup(url.text, "html.parser")
+def gpuScraper():
+    url = requests.get("https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html")
+    soup = BeautifulSoup(url.text, "html.parser")
 
-tableEntries = soup.findAll("td")
-lastGpu = soup.find("td", string=re.compile("GT 1030"))
-endList = tableEntries.index(lastGpu)
+    tableEntries = soup.findAll("td")
+    lastGpu = soup.find("td", string=re.compile("GT 1030"))
+    endList = tableEntries.index(lastGpu)
 
-gpuNames = tableEntries[0:endList+1:7]
-gpuFPS = tableEntries[2:endList+8:7]
+    gpuNames = tableEntries[0:endList+1:7]
+    gpuFPS = tableEntries[2:endList+8:7]
 
-for i in range(len(gpuFPS)):
-    if "fps" in gpuFPS[i].text:
-        print(gpuNames[i].text + ',', gpuFPS[i].text[gpuFPS[i].text.index('(')+1:gpuFPS[i].text.index('f')])
-    else:
-        print(gpuNames[i].text + ',', None)
+    dict = {}
+    gpus = {'1650','1660','1050','1060','1070','1080',
+            '2060','2070','2080',
+            '3050','3060','3070','3080','3090',
+            '4060','4070','4080','4090',
+            '5700','6600','6650','6700','6750','6800','6900','6850',
+            '7600','7700','7800','7900'
+            }
+
+    for i in range(len(gpuNames)):
+        for gpu in gpus:
+            if gpu in gpuNames[i].text.lower():
+                if f"{gpu} ti" in gpuNames[i].text.lower():
+                    gpu += " ti"
+                if f"{gpu} super" in gpuNames[i].text.lower():
+                    gpu += " super"
+                if f"{gpu} xtx" in gpuNames[i].text.lower():
+                    gpu += " xtx"
+                elif f"{gpu} xt" in gpuNames[i].text.lower():
+                    gpu += " xt"
+
+                if "fps" in gpuFPS[i].text:
+                    dict.update({gpu:float(gpuFPS[i].text[gpuFPS[i].text.index('(')+1:gpuFPS[i].text.index('f')])})
+
+    return dict
